6c4e89e773f50901a020503d301e1bfd5ebb0a83
AbdulRehman <likeam99@gmail.com>
2023-01-27 20:15:44 +0500

    Key board input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css"
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  const inputTask = useRef(null);
+
+  const addTask = () =>{
+    setTodoList([...todoList, {task: currentTask, completed: false}]);
+    inputTask.current.value = "";
+    setCurrentTask(""); 
+    console.log(todoList);
+  }
+
+  const deleteTask = (taskToDelete) => {
+    setTodoList(
+        todoList.filter((task) => {
+        return task.task !== taskToDelete;
+      })
+    )
+  }
+
+  const completeTask = (taskToComplete) => {
+    setTodoList(
+        todoList.map((task) => {
+        return (
+          task.task === taskToComplete 
+        ? {task:taskToComplete, completed: true} 
+        : {task:task.task, completed: task.completed ? true : false} 
+        )
+      })
+    )
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDoList</h1>
+      <div>
+        <input 
+        ref={inputTask}
+        type="text" placeholder="Task..." 
+        onChange={(event) => {
+          setCurrentTask(event.target.value)}}
+          onKeyDown = {(event) => {
+            if (event.keyCode === 13){
+              addTask();
+            }
+          }}
+          />
+        <button onClick={addTask}  >Add Task</button>
+      </div>
+      <hr />
+      <ul>
+        {todoList.map((val, key) =>{
+          return (
+            <div id="task">
+              <li key={key} > {val.task}</li>
+              <button onClick={()=> completeTask(val.task)}>Complete</button>
+              <button onClick={()=> deleteTask(val.task)}>X</button>
+              {val.completed ? <h1>Task Completed</h1> : <h1>Task is Imcomplete</h1>}
+            </div>
+          )
+        })}
+      </ul>
     </div>
   );
 }
